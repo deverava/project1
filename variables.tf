3823353efe8d67f2c040a24e777c1a8603f9de73
dev <dvchandra2@gmail.com>
2025-12-05 11:31:59 -0500

    Updated variables and workflow

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
-# Test change to trigger CI
-# trigger pipeline test
+# Region for all resources
 variable "location" {
   type        = string
   description = "Azure region for the deployment"
   default     = "westus2"
 }
 
+# Common tags
 variable "project_owner" {
   type        = string
   description = "Owner tag for all resources"
@@ -18,15 +18,16 @@ variable "environment" {
   default     = "PoC"
 }
 
+# VM admin username
 variable "vm_admin_username" {
   type        = string
   description = "Admin username for the VM"
   default     = "vmadmin"
 }
 
+# VM admin password (no default – must be provided)
 variable "vm_admin_password" {
   type        = string
   description = "Admin password for the VM"
   sensitive   = true
 }
-
