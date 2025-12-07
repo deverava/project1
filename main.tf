a9bdf04c406712d5e8209108873dd177628b3066
dev <dvchandra2@gmail.com>
2025-12-06 19:44:53 -0500

    Fix duplicate required_providers and use azurerm backend

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_version = ">= 1.7.0"
+
+  # Remote backend so your laptop and GitHub Actions share the same state
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate-vijay" # <- RG you created for state
+    storage_account_name = "vijaytfstate123"  # <- your storage account name
+    container_name       = "tfstate"          # <- container you created
+    key                  = "project1.terraform.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
 # Get info about the current Azure identity (who you logged in as)
 data "azurerm_client_config" "current" {}
 
